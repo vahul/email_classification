a6163f0f9067883681c2bc0de279100eae05a605
CHEMBETI VAHUL <98199660+vahul@users.noreply.github.com>
2024-12-13 16:04:42 +0530

    Update wsgi.py

--- email_classification/wsgi.py
+++ email_classification/wsgi.py
@@ -12,6 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'email_classification.settings')
-
 application = get_wsgi_application()
-app=application

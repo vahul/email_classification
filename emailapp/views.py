5b9ed6e8e9e95e849bf0d5affcd96bebd99932f4
CHEMBETI VAHUL <98199660+vahul@users.noreply.github.com>
2024-12-14 10:56:11 +0530

    Update views.py

--- emailapp/views.py
+++ emailapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 import os
 import base64
-from datetime import datetime
+from datetime import datetime,timedelta
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
